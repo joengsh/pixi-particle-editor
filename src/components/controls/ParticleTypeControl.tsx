93f886e4eff43fc53e7e5bb4ec5da8464d78a98d
Siu Hin Ng <siu.hin.ng@inseinc.com>
2026-04-13 14:58:31 +0100

    update ParticleBasicTypeControl

--- src/components/controls/ParticleTypeControl.tsx
+++ src/components/controls/ParticleTypeControl.tsx
@@ -8,14 +8,42 @@ import {
   Select,
 } from '../ui/select'
 import { useShallow } from 'zustand/shallow'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { Separator } from '../ui/separator'
 import type { ParticleTypeData } from '@/types/particleConfigUIData'
+import { MultiSelect } from '../MultiSelect'
+import useTextureStore from '@/stores/TextureStore'
 
 const particleTypes = ['basic', 'animated']
 
 const ParticleBasicTypeControl = () => {
-  return null
+  const textureData = useTextureStore(state=>state.textureData)
+  const particleType = useParticleConfigStore(useShallow(state=>state.configUI.particleType))
+  const setConfigUI = useParticleConfigStore(useShallow(state=>state.setConfigUI))
+  const options = useMemo(()=>Object.keys(textureData).map(textureName => ({value: textureName, label: textureName})), [textureData])
+  const [values, setValue] = useState<string[]>(particleType.art as string[])
+
+  useEffect(()=>{
+    setConfigUI({
+      particleType: {
+        type: 'basic',
+        art: values,
+        orderedArt: false
+      }
+    })
+  }, [values])
+
+  return (
+    <>
+      <MultiSelect
+        defaultValue={values}
+        options={options}
+        value={values}
+        onValueChange={setValue}
+        placeholder="Choose textures..."
+      />
+    </>
+  )
 }
 
 const ParticleAnimatedTypeControl = () => {
